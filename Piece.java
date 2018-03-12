495e7f19eba20ab16df59f743c8887f626c45887
Justin Knueppel <justinknueppel@gmail.com>
2018-03-12 18:34:16 -0400

    Create base interface for Piece

---
+++ Piece.java
@@ -0,0 +1,13 @@
+public interface Piece {
+    /**
+     * Moves the given piece to a new square.
+     * @param newSquare
+     *      The target square for this
+     */
+    void move(Square newSquare);
+
+    /**
+     * @updates this.possibleMoves with the possible moves
+     */
+    void updatePossibleMoves();
+}
