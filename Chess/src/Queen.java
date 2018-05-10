6e2b7f472430cb5dd41b9ff1bc2820042230739f
Justin Knueppel <justinknueppel@gmail.com>
2018-05-09 21:00:28 -0400

    Add move logic for Queen, consider using [][] notation for rook and bishop

--- Chess/src/Queen.java
+++ Chess/src/Queen.java
@@ -32,9 +32,31 @@ public class Queen implements Piece {
     }
 
     private void updatePossibleMoves() {
-
+        String curID = this.square.getID();
+        final int curRank = this.board.LETTERS.indexOf(curID.charAt(0));
+        final int curFile = this.board.NUMBERS.indexOf(curID.charAt(1));
+        int[][] directions = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        for(int[] adjustments : directions) {
+            boolean checkSquare = true;
+            int multiplier = 1;
+            while (checkSquare) {
+                checkSquare = addMoveIfLegal(curRank + adjustments[0] * multiplier, curFile + adjustments[1] * multiplier);
+                multiplier++;
+            }
+        }
     }
-
+    private boolean addMoveIfLegal(int rank, int file) {
+        boolean allowNext = false;
+        if (this.board.inBounds(rank) && this.board.inBounds(file)){
+            String idToCheck = this.board.indexToID(rank, file);
+            Square square = this.board.getSquare(idToCheck);
+            allowNext = square.isOccupied();
+            if (!square.isOccupied() || square.getPiece().getTeam() != this.team) {
+                this.moves.add(idToCheck);
+            }
+        }
+        return allowNext;
+    }
     @Override
     public ArrayList<String> getPossibleMoves() {
         updatePossibleMoves();
