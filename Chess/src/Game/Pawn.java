dc7b43ceda2b50c56cbd8d8ccfce79e0b3a60c5f
Justin Knueppel <justinknueppel@gmail.com>
2018-05-18 19:52:36 -0400

    Fix Pawn to comply - comment out move logic to move later

--- Chess/src/Game/Pawn.java
+++ Chess/src/Game/Pawn.java
@@ -1,52 +1,60 @@
 package Game;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-import java.util.ArrayList;
+import Game.Pieces.MoveType;
 
 public class Pawn extends Piece {
 
-    Pawn(Board board, TeamColor team) {
-        this.team = team;
-        this.hasMoved = false;
-        this.moves = new ArrayList<>();
-        this.board = board;
-        this.type = PieceType.PAWN;
-        this.URL = PRE_IMAGE + this.team.name() + this.type.name() + ".png";
-        initImage();
+    Pawn(Coordinates coordinates, TeamColor team) {
+        super(coordinates, team);
     }
+
     @Override
+    void initMoveDirections() {
+        int direction = team == TeamColor.WHITE ? 0 : 2;
+        this.moveDirections = new MoveType[][]{
+                {MoveType.NONE, MoveType.NONE, MoveType.NONE},
+                {MoveType.NONE, MoveType.NONE, MoveType.NONE},
+                {MoveType.NONE, MoveType.NONE, MoveType.NONE}};
+        this.moveDirections[direction][0] = MoveType.KILL;
+        this.moveDirections[direction][1] = MoveType.MOVE;
+        this.moveDirections[direction][2] = MoveType.KILL;
+    }
+
+    @Override
+    void initPieceType() {
+        this.type = PieceType.PAWN;
+    }
+/*@Override
     protected void updatePossibleMoves() {
         final int direction = (this.team == TeamColor.WHITE) ? 1 : -1;
         this.moves.clear();
         Coordinates id = this.square.getID();
         int nextRank = id.getRank() + direction;
         if (board.inBounds(nextRank)){
-            /*
+            *//*
             Check directly in front of the piece
-             */
+             *//*
             int curFile = id.getFile();
             Coordinates idToCheck = new Coordinates(nextRank, curFile);
             if(!board.getSquare(idToCheck).isOccupied()) {
                 this.moves.add(idToCheck);
-                /*
+                *//*
                 Check if moving twice is possible
-                 */
+                 *//*
                 if(!this.hasMoved && board.inBounds(nextRank + direction)) {
                     addMoveIfLegal(nextRank + direction, curFile, false);
                 }
             }
-            /*
+            *//*
             Check front left
-             */
+             *//*
             int leftFile = curFile - direction;
             if (board.inBounds(leftFile)) {
                 addMoveIfLegal(nextRank, leftFile, true);
             }
-            /*
+            *//*
             Check front right
-             */
+             *//*
             int rightFile = curFile + direction;
             if (board.inBounds(rightFile)){
                 addMoveIfLegal(nextRank, rightFile, true);
@@ -54,7 +62,7 @@ public class Pawn extends Piece {
         }
     }
 
-    /**
+    *//**
      * Given a rank and file, check if the move is legal.
      * @param rank
      *      Index of the alpha component of a tile
@@ -62,13 +70,13 @@ public class Pawn extends Piece {
      *      Index of the numeric component of a tile
      * @param capture
      *      Determines if a capture is legal
-     */
+     *//*
     private void addMoveIfLegal(int rank, int file, boolean capture) {
         Coordinates idToCheck = new Coordinates(rank, file);
         if (!board.getSquare(idToCheck).isOccupied()
                 || (capture && board.getSquare(idToCheck).getPiece().getTeam() != team)) {
             moves.add(idToCheck);
         }
-    }
+    }*/
 
 }
