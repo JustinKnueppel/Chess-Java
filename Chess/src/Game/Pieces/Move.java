792568fc194130b0f5fa3fcb7da4804e6ad8e2f4
Justin Knueppel <justinknueppel@gmail.com>
2018-05-25 10:49:47 -0400

    Add MoveType component to Move

--- Chess/src/Game/Pieces/Move.java
+++ Chess/src/Game/Pieces/Move.java
@@ -1,6 +1,7 @@
 package Game.Pieces;
 
 import Game.Coordinates;
+import Game.MoveType;
 import Game.Square;
 
 public final class Move {
@@ -8,12 +9,14 @@ public final class Move {
     private Piece newPiece;
     private Coordinates oldCoordinates;
     private Coordinates newCoordinates;
+    private MoveType moveType;
 
-    public Move(Piece movingPiece, Square newSquare) {
+    public Move(Piece movingPiece, Square newSquare, MoveType moveType) {
         this.newPiece = movingPiece;
         this.newCoordinates = newSquare.getID();
         this.oldCoordinates = movingPiece.getCoordinates();
         this.oldPiece = newSquare.isOccupied() ? newSquare.getPiece() : null;
+        this.moveType = moveType;
     }
     public Piece getOldPiece() {
         return oldPiece;
@@ -30,5 +33,9 @@ public final class Move {
     public Coordinates getOldCoordinates() {
 
         return oldCoordinates;
+    }
+
+    public MoveType getMoveType() {
+        return moveType;
     }
 }
