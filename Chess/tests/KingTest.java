598f43c1d1a6748756e72c40b20a4a51925a962b
Justin Knueppel <justinknueppel@gmail.com>
2018-07-20 18:40:19 -0400

    Create more tests - issue with dynamic tests being mutable

--- Chess/tests/KingTest.java
+++ Chess/tests/KingTest.java
@@ -1,6 +1,7 @@
 import Game.Board;
 import Game.Coordinates;
 import Game.MoveType;
+import Game.Pieces.Pawn;
 import Game.Pieces.Piece;
 import Game.Pieces.PieceType;
 import Game.TeamColor;
@@ -29,7 +30,7 @@ class KingTest {
     }
 
     @TestFactory
-    public Collection<DynamicTest> testMoves() {
+    public Collection<DynamicTest> testLegalMoves() {
 
         Collection<DynamicTest> dynamicTests = new ArrayList<>();
 
@@ -38,9 +39,9 @@ class KingTest {
             MoveType moveType = board.isLegalMove(piece, newCoords);
             board.move(piece, newCoords, moveType);
 
-            Executable exec = () -> assertEquals(moveType, MoveType.NORMAL);
+            Executable exec = () -> assertEquals(MoveType.NORMAL, moveType);
 
-            String testName = "Testing move to X:" + newCoords.getX() + " Y:" + newCoords.getY();
+            String testName = "Testing MoveType of move to X:" + newCoords.getX() + " Y:" + newCoords.getY();
 
             DynamicTest dynamicTest = DynamicTest.dynamicTest(testName, exec);
 
@@ -50,6 +51,86 @@ class KingTest {
         }
 
         return dynamicTests;
+    }
+
+    @TestFactory
+    public Collection<DynamicTest> testLegalCaptures() {
+
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int[] move : piece.getPossibleMoves()) {
+            Coordinates newCoords = new Coordinates(piece.getCoordinates().getX() + move[0], piece.getCoordinates().getY() + move[1]);
+            Piece enemyPiece = new Pawn(newCoords, TeamColor.BLACK);
+            board.getGrid()[newCoords.getX()][newCoords.getY()].putPiece(enemyPiece);
+            MoveType moveType = board.isLegalMove(piece, newCoords);
+            board.move(piece, newCoords, moveType);
+
+            Executable exec = () -> assertEquals(MoveType.NORMAL, moveType);
+
+            String testName = "Testing MoveType of capture of pawn on X:" + newCoords.getX() + " Y:" + newCoords.getY();
+
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(testName, exec);
+
+            dynamicTests.add(dynamicTest);
+            board.revertMove();
+
+        }
+
+        return dynamicTests;
+    }
+    @TestFactory
+    public Collection<DynamicTest> testMovePlacement() {
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int[] move : piece.getPossibleMoves()) {
+            Coordinates newCoords = new Coordinates(piece.getCoordinates().getX() + move[0], piece.getCoordinates().getY() + move[1]);
+            MoveType moveType = board.isLegalMove(piece, newCoords);
+            board.move(piece, newCoords, moveType);
+
+            Executable exec = () -> assertEquals(piece, board.getGrid()[newCoords.getX()][newCoords.getY()].getPiece());
+
+            String testName = "Testing move of piece to X: " + newCoords.getX() + " Y: " + newCoords.getY();
+
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(testName, exec);
+
+            dynamicTests.add(dynamicTest);
+            board.revertMove();
+        }
+        return dynamicTests;
+    }
+    @TestFactory
+    public Collection<DynamicTest> testCaptureMovePlacement() {
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int[] move : piece.getPossibleMoves()) {
+            Coordinates newCoords = new Coordinates(piece.getCoordinates().getX() + move[0], piece.getCoordinates().getY() + move[1]);
+            Piece enemyPiece = new Pawn(newCoords, TeamColor.BLACK);
+            board.getGrid()[newCoords.getX()][newCoords.getY()].putPiece(enemyPiece);
+            MoveType moveType = board.isLegalMove(piece, newCoords);
+            board.move(piece, newCoords, moveType);
+
+            Executable exec = () -> assertEquals(piece, board.getGrid()[newCoords.getX()][newCoords.getY()].getPiece());
+
+            String testName = "Testing capture of enemy pawn by piece to X: " + newCoords.getX() + " Y: " + newCoords.getY();
+
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(testName, exec);
+
+            dynamicTests.add(dynamicTest);
+            board.revertMove();
+        }
+        return dynamicTests;
+    }
+    @Test
+    public void testOneMove() {
+        Coordinates newCoords = new Coordinates(4, 5);
+        Piece enemyPiece = new Pawn(newCoords, TeamColor.BLACK);
+        board.getGrid()[newCoords.getX()][newCoords.getY()].putPiece(enemyPiece);
+
+        MoveType moveType = board.isLegalMove(piece, newCoords);
+        board.move(piece, newCoords, moveType);
+
+        assertEquals(MoveType.NORMAL, moveType);
+        assertEquals(piece, board.getGrid()[newCoords.getX()][newCoords.getY()].getPiece());
     }
 
 }
