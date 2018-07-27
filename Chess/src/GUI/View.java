4f10d35fb19f076e1eb8d98aa6a331acfcdea671
Hoopsters35 <32350916+Hoopsters35@users.noreply.github.com>
2018-07-27 18:04:26 -0400

    Add a main method to allow project to work in eclipse

--- Chess/src/GUI/View.java
+++ Chess/src/GUI/View.java
@@ -34,6 +34,10 @@ public class View extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
+    public static void main(String[] args) {
+    	launch(args);
+    }
 
 
 
